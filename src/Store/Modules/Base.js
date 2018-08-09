6275135bafbfcfc3da233485a3e4039fcd51b352
Ciao Chung <ciao0958@gmail.com>
2018-08-10 01:21:12 +0800

    add loading

--- src/Store/Modules/Base.js
+++ src/Store/Modules/Base.js
@@ -1,6 +1,21 @@
 export default {
-  state: {},
-  mutations: {},
-  actions: {},
-  getters: {},
+  namespaced: true,
+  state: {
+    loading: false,
+  },
+  mutations: {
+    loading(state, status = true) {
+      state.loading = status
+    },
+  },
+  actions: {
+    loading(context, status) {
+      context.commit('loading', status)
+    },
+  },
+  getters: {
+    loading(state) {
+      return state.loading
+    },
+  },
 }
